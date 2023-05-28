91058e4d6d0fa8115d782ab5142095b3d4c07984
mohsen-pashootan <tahmasebpour.mohsen@gmail.com>
2023-05-28 21:56:49 +0330

    init project

--- util/date.js
+++ util/date.js
@@ -1,5 +1,5 @@
 export function getFormattedDate(date) {
-  return date.toISOString().slice(0, 10);
+  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 }
 
 export function getDateMinusDays(date, days) {
